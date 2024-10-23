10f73aa610a42086937f055822cbd54d417bb35e
argonised <ankitreddy82@gmail.com>
2024-10-23 09:48:18 +0100

    adding in basketball score-board task
    made a basketball score-board as part of the scrimba task. took me 45 mins including a call from my dad halfway through where I showed him my progress. super proud of myself

--- functionality.js
+++ functionality.js
@@ -1,6 +1,39 @@
-let text = "let's go!"
-let moddedText = text
+let moddedText = "let's go!"
  function addExc() {
     moddedText = moddedText + "!"
     document.getElementById("btn-exc").textContent = moddedText
+}
+
+let homeScore = 0
+let awayScore = 0
+
+function homePlusOne() {
+    homeScore += 1
+    document.getElementById("home-score").textContent = homeScore
+}
+function awayPlusOne() {
+    awayScore += 1
+    document.getElementById("away-score").textContent = awayScore
+}
+function homePlusTwo() {
+    homeScore += 2
+    document.getElementById("home-score").textContent = homeScore
+}
+function awayPlusTwo() {
+    awayScore += 2
+    document.getElementById("away-score").textContent = awayScore
+}
+function homePlusThree() {
+    homeScore += 3
+    document.getElementById("home-score").textContent = homeScore
+}
+function awayPlusThree() {
+    awayScore += 3
+    document.getElementById("away-score").textContent = awayScore
+}
+function reset() {
+    homeScore = 0
+    document.getElementById("home-score").textContent = homeScore
+    awayScore = 0
+    document.getElementById("away-score").textContent = awayScore
 }
